5f89d05b27d8e45f0ac90e4c2fd8e8d923c0076d
Leejjon <leejjon@gmail.com>
2021-02-07 11:58:28 +0100

    Added two tests and improved naming.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,15 +5,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+    @Test
+    void testUpdateQuality_verifyQualityAndSellInDateDecrease_forStandardItems() {
+        Item[] items = new Item[] { new Item("foo", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(4, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    }
 
     @Test
-    void testUpdateQuality_ItemFooSellIn0Quality0_success() {
+    void testUpdateQuality_verifyQualityDoesntGetNegative_WhenPassingSellInDate() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-
         assertEquals("foo", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void testUpdateQuality_proveQualityIncreasesTwiceAsFast_AfterSellInReachesZero() {
+        Item[] items = new Item[] { new Item("food", 0, 10) };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("food", app.items[0].name);
+        assertEquals(8, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
     }
 
 }
