2eed65fbede113fa1307ef85bfe043e40634a976
Leejjon <leejjon@gmail.com>
2021-02-07 17:39:21 +0100

    Added the maxQuality to the LimitedDateItemUpdater.

--- src/main/java/com/gildedrose/items/LimitedDateItemUpdater.java
+++ src/main/java/com/gildedrose/items/LimitedDateItemUpdater.java
@@ -4,22 +4,33 @@ import com.gildedrose.Item;
 import com.gildedrose.ItemUpdater;
 
 public class LimitedDateItemUpdater implements ItemUpdater {
+    private static final int maxQuality = 50;
+
     @Override
     public Item passDay(Item item) {
         int sellIn = item.sellIn;
         int quality = item.quality;
 
         if (sellIn > 10) {
-            quality++;
+            quality = incrementQuality(quality, 1);
         } else if (sellIn > 5) {
-            quality += 2;
+            quality = incrementQuality(quality, 2);
         } else if (sellIn > 0) {
-            quality += 3;
+            quality = incrementQuality(quality, 3);
         } else {
             quality = 0;
         }
         sellIn--;
 
         return new Item(item.name, sellIn, quality);
+    }
+
+    private int incrementQuality(int quality, int increment) {
+        final int incrementedValue = quality + increment;
+        if (incrementedValue >= maxQuality) {
+            return maxQuality;
+        } else {
+            return incrementedValue;
+        }
     }
 }
